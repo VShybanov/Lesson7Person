69d0f8f77b496fcd4dca782d233993de085e5e58
Vitalii Shybanov <vs.shibanov@gmail.com>
2023-10-20 21:05:08 +0300

    Add static method personInfo

--- src/Lesson7Person.java
+++ src/Lesson7Person.java
@@ -1,8 +1,8 @@
 public class Lesson7Person {
     public static void main(String[] args) {
         Person person = new Person();
-        System.out.println(person.personInfo("Will","Smith","New York","2936729462846"));
-        System.out.println(person.personInfo("Jackie","Chan","Shanghai","12312412412"));
-        System.out.println(person.personInfo("Sherlock","Holmes","London","37742123513"));
+        System.out.println(person.personInfo("Will","Smith","New York",293672946));
+        System.out.println(Person.personInfoStatic("Jackie","Chan","Shanghai",1231241241));
+        System.out.println(person.personInfo("Sherlock","Holmes","London",377421235));
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,10 @@
 public class Person {
-    private final String template = "Зателефонувати громадянину %s %s із міста %s за номером %s";
-    public String personInfo(String firstName, String lastName, String city, String telNumber) {
+    private static final String template = "Зателефонувати громадянину %s %s із міста %s за номером %d";
+    public String personInfo(String firstName, String lastName, String city, long telNumber) {
+        return String.format(template, firstName, lastName, city, telNumber);
+    }
+
+    public static String personInfoStatic(String firstName, String lastName, String city, long telNumber) {
         return String.format(template, firstName, lastName, city, telNumber);
     }
 }
